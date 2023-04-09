03a7200ba8541de699d6a60de79357f5c6508a5a
esctabcapslock <62400002esctabcapslock@users.noreply.github.com>
2023-04-09 21:38:23 +0900

    Initial Commit

---
+++ myMp3TagEditor/myMp3TagEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMp3TagEditorApp.swift
+//  myMp3TagEditor
+//
+//  Created by zffridption on 2023. 4. 9..
+//
+
+import SwiftUI
+
+@main
+struct myMp3TagEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myMp3TagEditorUITests/myMp3TagEditorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  myMp3TagEditorUITestsLaunchTests.swift
+//  myMp3TagEditorUITests
+//
+//  Created by zffridption on 2023. 4. 9..
+//
+
+import XCTest
+
+final class myMp3TagEditorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
